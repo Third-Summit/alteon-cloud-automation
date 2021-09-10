2c12313241d3f6e164b9059dda51a4eea3d6d6a1
Duha <85308243+Duha-ArafChowdhury@users.noreply.github.com>
2021-09-10 14:17:38 -0400

    new class

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		
-		features = "src/test/java/Features/LoginLogout.feature", //"src/test/java/Features/feature name if you want to run a specific feature
+		features = "src/test/java/Features/ShareProject.feature", //"src/test/java/Features/feature name if you want to run a specific feature
 		glue = "stepDefinition",
 		monochrome = true, //displays console output in a readable format
 		//strict = true,//it will check if steps are not defined in step definition file
